3d9ec119cda6d04fc6c1945bf6e672d9a18dc696
Uchchash346 <mru416@gmail.com>
2022-05-08 11:58:08 +0600

    Styling in second component and within component

--- src/app/components/within-component/within-component.component.ts
+++ src/app/components/within-component/within-component.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-within-component',
   template: '<h2>I am within component</h2>',
-  styleUrls: ['./within-component.component.css']
+  styles: ['h2 {font-weight: normal; color: red}']
 })
 export class WithinComponentComponent implements OnInit {
 
